2518d2c8381c8d95a513ed8b4af6191dec9fb4f0
Maor Kleinberger <kmaork@gmail.com>
2023-12-06 20:21:55 +0200

    Update python version (#2)

--- app/setup.py
+++ app/setup.py
@@ -4,7 +4,7 @@ setup(
     name='microserver',
     version='0.1.0',
     packages=find_packages(exclude=('tests',)),
-    install_requires=['flask==2.0.3'],
+    install_requires=['flask==2.0.3', 'Werkzeug==2.2.2'],
     python_requires='>=3.7',
     entry_points=dict(
         console_scripts=['microserver=microserver.__main__:main']
